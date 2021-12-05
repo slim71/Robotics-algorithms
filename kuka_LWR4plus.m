b8e1137e15de6a97363842e42c9bb67bc22e83f4
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2021-12-05 13:18:22 +0100

    Update kuka_LWR4plus.m

--- kuka_LWR4plus.m
+++ kuka_LWR4plus.m
@@ -1,3 +1,4 @@
+%% Preparations
 clc; clear all; close all;
 
 syms t
@@ -5,24 +6,9 @@ syms q [7 1]
 % TODO: assumptions?
 
 %% Numerical Data
-l0 = 0.11; l1 = 0.20; l2 = 0.20; l3 = 0.20; l4 = 0.20; l5 = 0.19; offset = 0.078;
+l0 = 11; l1 = 20; l2 = 20; l3 = 20; l4 = 20; l5 = 19; offset = 7.8;
 
-rho = 0.05; % [m]
-
-%% Main trajectory
-t_heli = 0:pi/10:3*pi;
-x_heli = rho*sin(t_heli);
-y_heli = rho*cos(t_heli);
-z_heli = 0.6:0.2/30:0.8;
-
-traj_fig = figure2();
-grid
-hold on
-plot3(x_heli, y_heli, z_heli);
-for k = 1:size(t_heli,2)
-    plot3(x_heli(k), y_heli(k), z_heli(k),'x--','Color','blue')
-end
-view(50,60)
+rho = 10; % [cm]
 
 %% Denavit-Hartemberg and Jacobian
 %                a   alpha   d       theta    joint_type
@@ -36,77 +22,38 @@ denavit = @(q)([[0,  pi/2,   0,      q(1),       "R"]
 
 Jac = @(q, ind)(JacobFromDH(denavit(q), ind));
 
-J = @(q)Jac(q,0);
-J4 = @(q)Jac(q, 4);
+J = @(q)Jac(q,0); % J4 = @(q)Jac(q, 4);
+
+Tos = HomX(0, [0,0,l0+l1]);
+ForKine = @(q) Tos*DHFKine(denavit(q));
 
 %% Tasks
-% Desired EE motion: helicoidal
+t_task = 0:pi/10:3*pi;
 
-despos = @(t) [rho*sin(t), rho*cos(t), t, 0, 0, 0];
-destwist = @(t) diff(despos(t));
+% Desired EE motion
+despose = @(t) [20 - 10*sqrt(3)*sin(t), 20*cos(t), 65 + 10*sin(t),0.5*t,0,sqrt(3)/2*t];
 
-% Desired Joint 4 motion: stable in place
-% despos4 = @(t) [0; 0; 0; 0; 0; 0];
-% destwist4 = @(t) diff(despos4);
+% Make actual derivative of function, avoiding 'diff' problems
+destwist = matlabFunction(diff(despose(t)));
 
-% Oee(t) = [rho*cos(t); rho*sin(t)+l4; l0+l1+l2+l3];%t in terza comp
-% OeeHat(t) = AxisToSkew(Oee(t));
-% Moee(t) = [[eye(3),     OeeHat(t)];
-%            [zeros(3,3), eye(3)   ]];
-
-% Oo4 = [0; 0; l0+l1+l2+l3];
-% Oo4Hat = AxisToSkew(Oo4);
-% Mo4 = [[eye(3),     Oo4Hat];
-%        [zeros(3,3), eye(3)]];
-
-% In global coordinates
-% twistEE_O(t) = Moee(t)*twistEE(t);
-% twist4_O = Mo4*twist4;
-
-% Complete partial jacobian
-J_4 = [J4(q), zeros(size(J4(q),1), size(J(q),2)-size(J4(q),2))];
-
-Rsdes = @(q) RotZ(pi/2);
-
-% Tw(q) = [Twphi1,Twphi2,Twphi3]
-
-% Just use geometric J but compute orientation error with something else 
-% (quaternions..) --> look on Sciavicco
-TA = [[eye(3),      zeros(3,3)  ];
-      [zeros(3,3),  zeros(3,3)]];%Twphi       ]];
-
-Janal = @(q) (TA*J(q));
-
-% jacobs = cat(3,J(q),J_4);
-
-qSotNorm = sot(J(q), ...
-               destwist(t)',..., twist4_O],...
-               0);
-
-% qSotNorm = sot(J(q), ...
-% qSotNorm = sot(Janal(q), ...
-%                [twistEE_O(t)],..., twist4_O],...
-%                0);
-
-%% Plot solution
-f1 = figure2();
-
-for j=[1:size(qSotNorm,1)]
-    subplot(size(qSotNorm,1), 1, j)
-    plot(squeeze(qSotNorm(j,end,:)))
+traj_fig = figure2('Name', 'Task trajectory');
+grid
+hold on
+first_traj = [];
+for k = t_task
+    first_traj = [first_traj; despose(k)];
 end
+plot3(first_traj(:,1),first_traj(:,2),first_traj(:,3),'-')
+view(45,45)
 
 %% Robot design
 % TODO: build upon denavit
 kuka = rigidBodyTree('Dataformat', 'column');
 
-dhparams = [0  pi/2   0         qSotNorm(1,end,1); 
-            0  -pi/2  0         qSotNorm(2,end,1);
-            0  -pi/2  l2+l3     qSotNorm(3,end,1);
-            0  pi/2   0         qSotNorm(4,end,1);
-            0  pi/2   l4+l5     qSotNorm(5,end,1);
-            0  -pi/2  0         qSotNorm(6,end,1);
-            0  0      offset    qSotNorm(7,end,1)];
+% It's sufficient to drop the last column and give whatever values to q:
+% joint variables are ignored in setFixedTransform
+dhparams = double(denavit(zeros(7,1)));
+dhparams = dhparams(:,1:end-1);
 
 bodyNames = {'b1','b2','b3','b4','b5', 'b6','b7'};
 parentNames = {'base','b1','b2','b3','b4', 'b5','b6'};
@@ -126,46 +73,151 @@ end
 
 showdetails(kuka)
 
-% f = figure2('Name', 'Using solution');
-% % TODO: maybe in a while with a constant check to a flag to repeat?
-% % or else, continuously in a thread?
-% for i=1:size(qSotNorm,3)
-%     gr = show(kuka,qSotNorm(:,end,i));
-%     view(-50, 60);
-%     drawnow
-%     pause(0.1)
-% end
+%% Needed functions
+curr_pos = @(q) indexAt(ForKine(q),1:3,4);
+des_pos = @(t) indexAt(despose(t), 1:3);
+pos_error = @(t,q) (des_pos(t) - curr_pos(q)');
 
-%% Integration
-ris = cumtrapz(0.01, qSotNorm, 3);
+curr_or = @(q) MatToQuat(indexAt(ForKine(q),1:3,1:3));
+des_or = @(t) EulToQuat(indexAt(despose(t),4:6),'ZYX',false);
+or_error = @(t, q) indexAt(QuatProd(des_or(t),QuatInv(curr_or(q))),2:4);
 
-fris = figure2('Name', 'with integration');
-for j=1:size(ris,3)
-    show(kuka, ris(:,end,3))
-    view(-50, 60);
+tot_errors = @(t,q) [pos_error(t,q), or_error(t,q)];
+
+%% IK parameters
+qinit = [0.5; 0.2; 0.2; pi/3; 0; pi/3; -0.8];
+
+t0 = 0;
+tf = 10;
+
+gains_cwe = 0.1 * [1 1 1 1 1 1];
+
+%% Clik kinematics
+
+[t_clik, y_clik] = ode45(@(t,y) clik(t,y,J,destwist), [t0 tf], qinit);
+q_clik = y_clik';
+
+ode_fig = figure2('Name', 'Clik with ode45');
+for i=[1:size(q_clik, 2)]
+    plot3(first_traj(:,1),first_traj(:,2),first_traj(:,3),'-', 'Color', 'black')
+    hold on
+    grid
+    show(kuka, q_clik(:,i));
+    view(45,45)
     drawnow
     pause(0.1)
+    hold off
+end
+clik_fig = figure2('Name', 'Clik solution');
+for j=[1:size(q_clik,1)]
+    subplot(size(q_clik,1), 1, j)
+    plot(q_clik(j,:))
 end
 
-hold on
-for t=[0:0.1:2*pi]
-    plot3(x_heli, y_heli, z_heli);
+%% Clik errors
+pe_fig = figure2('Name', 'Clik position error');
+p1 = [];
+p2 = [];
+p3 = [];
+for i=[1:size(t_clik,1)]
+    pe = pos_error(t_clik(i), q_clik(:,i));
+    p1 = [p1, pe(1)];
+    p2 = [p2, pe(2)];
+    p3 = [p3, pe(3)];
 end
 
-%% Trajectory
-% fig_heli = figure2();
-% view(-50, 60);
-% hold on
-% grid
-% show(kuka, homeConfiguration(kuka))
-
-fig_traj = figure('Name', 'traj attempt with integration');
-for curr_time_step = [1:63]
-    timestep = curr_time_step * 0.1;
-    xx = double(subs(destwist(t),t,timestep));
-    diff_traj(:,curr_time_step) = xx(1:3)';
+for i=[1:size(t_clik,1)]   
+    subplot(3, 1, 1);
+    plot(t_clik, p1);
+    subplot(3, 1, 2);
+    plot(t_clik, p2);
+    subplot(3, 1, 3);
+    plot(t_clik, p3);
 end
 
-% integration
-maybe_traj = cumtrapz(0.1, diff_traj,2);
-plot3(maybe_traj(:,1), maybe_traj(:,2), maybe_traj(:,3))
+% Orientation error computed using quaternions
+
+or_fig = figure2('Name', 'Clik orientation error');
+o1 = [];
+o2 = [];
+o3 = [];
+for i=[1:size(t_clik,1)]
+    oe = or_error(t_clik(i), q_clik(:,i));
+    o1 = [o1, oe(1)];
+    o2 = [o2, oe(2)];
+    o3 = [o3, oe(3)];
+end
+
+for i=[1:size(t_clik,1)]   
+    subplot(3, 1, 1);
+    plot(t_clik, o1);
+    subplot(3, 1, 2);
+    plot(t_clik, o2);
+    subplot(3, 1, 3);
+    plot(t_clik, o3);
+end
+
+%% Clik with errors
+[t_cwe, y_cwe] = ode45(@(t,y) clik_with_error(t,y,J,destwist,tot_errors, gains_cwe),...
+                       [t0 tf], qinit);
+q_cwe = y_cwe';
+
+ode_fig2 = figure2('Name', 'Cwe with ode45');
+for i=[1:size(q_cwe, 2)]
+    plot3(first_traj(:,1),first_traj(:,2),first_traj(:,3),'-', 'Color', 'black')
+    hold on
+    grid
+    show(kuka, q_cwe(:,i));
+    view(45,45)
+    drawnow
+    pause(0.1)
+    hold off
+end
+cwe_fig = figure2('Name', 'CWE solution');
+for j=[1:size(q_cwe,1)]
+    subplot(size(q_cwe,1), 1, j)
+    plot(q_cwe(j,:))
+end
+
+%% Clik_with_errors errors
+pe_fig = figure2('Name', 'CWE position error');
+p1 = [];
+p2 = [];
+p3 = [];
+for i=[1:size(t_cwe,1)]
+    pe = pos_error(t_cwe(i), q_cwe(:,i));
+    p1 = [p1, pe(1)];
+    p2 = [p2, pe(2)];
+    p3 = [p3, pe(3)];
+end
+
+for i=[1:size(t_cwe,1)]   
+    subplot(3, 1, 1);
+    plot(t_cwe, p1);
+    subplot(3, 1, 2);
+    plot(t_cwe, p2);
+    subplot(3, 1, 3);
+    plot(t_cwe, p3);
+end
+
+% Orientation error computed using quaternions
+
+or_fig = figure2('Name', 'Orientation error');
+o1 = [];
+o2 = [];
+o3 = [];
+for i=[1:size(t_cwe,1)]
+    oe = or_error(t_cwe(i), q_cwe(:,i));
+    o1 = [o1, oe(1)];
+    o2 = [o2, oe(2)];
+    o3 = [o3, oe(3)];
+end
+
+for i=[1:size(t_cwe,1)]   
+    subplot(3, 1, 1);
+    plot(t_cwe, o1);
+    subplot(3, 1, 2);
+    plot(t_cwe, o2);
+    subplot(3, 1, 3);
+    plot(t_cwe, o3);
+end
