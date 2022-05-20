22b34060bf572817c1dd688f393fe33092e1195b
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-05-20 17:49:10 +0200

    Fixed task definition
    Defined a more realistic task orientation.
    Recomputed solutions and errors.
    Improved readability.

--- kuka_LWR4plus.m
+++ kuka_LWR4plus.m
@@ -8,6 +8,13 @@ syms q [7 1]
 %% Numerical Data [cm]
 l0 = 0.11; l1 = 0.20; l2 = 0.20; l3 = 0.20; l4 = 0.20; l5 = 0.19;
 
+%% IK parameters
+qinit = [-pi/2, pi/4, 0, pi/3,0,0,0];
+
+t0 = 0;
+tf = 7;
+t_circle = 0:pi/10:3*pi;
+
 %% Denavit-Hartemberg and Jacobian
 %                a   alpha   d       theta    joint_type
 denavit = @(q)([[0,  pi/2,   0,      q(1),       "R"] 
@@ -24,31 +31,110 @@ J4 = @(q) DHJacob0(denavit(q), 4);
 ForKine = @(q) DHFKine(denavit(q));
 FK4 = @(q) DHFKine(denavit(q), 4);
 
+%% End-effector functions
+% Using matlabFunction(diff()) to make actual derivative of function, 
+% avoiding 'diff' classic problems.
+
+% Desired EE position
+EE_des_pos = @(t) [0.2 * sin(t), 0.2 * cos(t), 0.65];
+
+% Tanget and normal versor to the EE task
+% (and subsequent perpendicular axis)
+tan_vect = matlabFunction(diff(EE_des_pos(t)));
+vect_module = @(t) sqrt(indexAt(tan_vect(t), 1)^2 + ...
+                       indexAt(tan_vect(t),2)^2 + ...
+                       indexAt(tan_vect(t),3)^2);
+tan_vers = @(t) tan_vect(t)/vect_module(t);
+norm_vect = matlabFunction(diff(tan_vers(t)));
+norm_module = @(t) sqrt(indexAt(norm_vect(t), 1)^2 + ...
+                       indexAt(norm_vect(t),2)^2 + ...
+                       indexAt(norm_vect(t),3)^2);
+norm_vers = @(t) norm_vect(t)/norm_module(t);
+z_vers = @(t) cross(tan_vers(t), norm_vers(t));
+
+% Desired EE orientation
+EE_Rdes = @(t) [tan_vers(t)', norm_vers(t)', z_vers(t)'];
+EE_des_or = @(t) MatToRPY(EE_Rdes(t));
+
+% Desired EE pose
+EE_despose = @(t) [EE_des_pos(t), EE_des_or(t)];
+
+% Desired EE twist
+EE_v_des = matlabFunction(diff(EE_des_pos(t))); % linear velocity
+EE_Rdesdot = matlabFunction(diff(EE_Rdes(t)));
+EE_w_des = @(t) HatInv(EE_Rdesdot(t)*EE_Rdes(t)'); % angular velocity
+EE_destwist = @(t) [EE_v_des(t), EE_w_des(t)];
+
+% EE position error
+EE_curr_pos = @(q) indexAt(ForKine(q), 1:3, 4)';
+EE_pos_error = @(t, q) (EE_des_pos(t) - EE_curr_pos(q));
+
+% EE orientation error
+EE_curr_or_quat = @(q) MatToQuat(indexAt(ForKine(q), 1:3, 1:3));
+EE_des_or_quat = @(t) EulToQuat(indexAt(EE_despose(t), 4:6), 'ZYX', false);
+EE_or_error = @(t, q) indexAt(QuatProd(EE_des_or_quat(t), QuatInv(EE_curr_or_quat(q))), 2:4);
+
+% EE pose error
+EE_pose_errors = @(t, q) [EE_pos_error(t, q), EE_or_error(t, q)];
+
+%% 4th joint functions
+% Some functions have t and q as arguments only for consistency
+
+% 4th joint motion
+J4_des_pos = @(t) indexAt(FK4(qinit), 1:3, 4)';
+J4_des_or = @(t) MatToEulZYX(indexAt(FK4(qinit), 1:3, 1:3));
+J4_despose = @(t) [J4_des_pos(t), J4_des_or(t)];
+
+% J4_v_des = @(t) [0, 0, 0];
+% J4_w_des = @(t) [0, 0, 0]; % even using Rdesdot, it would be 0s
+% J4_destwist4 = @(t) [J4_v_des, J4_w_des];
+J4_destwist4 = @(t, q) [0 0 0 0 0 0];
+
+J4_curr_pos = @(q) indexAt(FK4(q), 1:3, 4)';
+% J4_des_pos = @(t) indexAt(J4_despose(t), 1:3);
+J4_pos_error = @(t, q) (J4_des_pos(t) - J4_curr_pos(q));
+
+J4_curr_or_quat = @(q) MatToQuat(indexAt(FK4(q), 1:3, 1:3));
+J4_des_or_quat = @(t) EulToQuat(indexAt(J4_despose(t), 4:6), 'ZYX', false);
+J4_or_error = @(t, q) indexAt(QuatProd(J4_des_or_quat(t), QuatInv(J4_curr_or_quat(q))), 2:4);
+
+J4_pose_errors = @(t, q) [J4_pos_error(t, q), J4_or_error(t, q)];
+
 %% Tasks
-t_task = 0:pi/10:3*pi;
 
-% Desired EE motion
-despose = @(t) [0.1 * sin(t), 0.2 * cos(t), 0.65, t, t, t];
-% Make actual derivative of function, avoiding 'diff' problems
-destwist = matlabFunction(diff(despose(t)));
-
-% As function of t and q only for consistency
-despose4 = @(t) [0 0 0 0 0 0];
-destwist4 = @(t,q) [0 0 0 0 0 0];
-
+% Graphical representation of the circle describing the EE main task
 traj_fig = figure2('Name', 'Task trajectory');
-title("Trajectory");
+title("Main task trajectory");
 grid
 hold on
 first_traj = [];
-for k = t_task
-    first_traj = [first_traj; despose(k)];
+for k = t_circle
+    first_traj = [first_traj; EE_despose(k)];
 end
 plot3(first_traj(:, 1), first_traj(:, 2), first_traj(:, 3), '-')
+xlabel('x');ylabel('y');zlabel('z');
 view(45, 45)
 
+% Draw an example of the desired EE orientation
+init_p = indexAt(EE_despose(0),1:3);
+x_dir = tan_vers(k)';
+y_dir = norm_vers(k)';
+z_dir = z_vers(k)';
+
+quiv_x = quiver3(init_p(1), init_p(2), init_p(3), x_dir(1), x_dir(2), x_dir(3));
+quiv_x.Color = 'red';
+quiv_y = quiver3(init_p(1), init_p(2), init_p(3), y_dir(1), y_dir(2), y_dir(3));
+quiv_y.Color = 'green';
+quiv_z = quiver3(init_p(1), init_p(2), init_p(3), z_dir(1), z_dir(2), z_dir(3));
+quiv_z.Color = 'blue';
+
+% Mathematical formulas for the two tasks
+K = 50 * eye(6);
+K2 = 50 * eye(6);
+task1 = @(t, q) (K * EE_pose_errors(t, q)')';
+task2 = @(t, q) (K2 * J4_pose_errors(t, q)')';
+
 %% Robot design
-% TODO: build upon denavit
 kuka = rigidBodyTree('Dataformat', 'column');
 
 % It's sufficient to drop the last column and give whatever values to q:
@@ -77,50 +163,10 @@ end
 
 showdetails(kuka)
 
-%% Needed functions
-curr_pos = @(q) indexAt(ForKine(q), 1:3, 4);
-des_pos = @(t) indexAt(despose(t), 1:3);
-pose_error = @(t, q) (des_pos(t) - curr_pos(q)');
-J4_pos = @(q) indexAt(FK4(q), 1:3, 4);
-J4_despos = @(t) indexAt(despose4(t), 1:3);
-J4_pose_error = @(t, q) (J4_despos(t) - J4_pos(q)');
-
-curr_or = @(q) MatToQuat(indexAt(ForKine(q),1:3,1:3));
-des_or = @(t) EulToQuat(indexAt(despose(t),4:6), 'ZYX', false);
-or_error = @(t, q) indexAt(QuatProd(des_or(t), QuatInv(curr_or(q))), 2:4);
-j4_or = @(q) MatToQuat(indexAt(FK4(q),1:3,1:3));
-J4_desor = @(t) EulToQuat(indexAt(despose4(t),4:6), 'ZYX', false);
-J4_orerr = @(t, q) indexAt(QuatProd(J4_desor(t), QuatInv(j4_or(q))), 2:4);
-
-tot_errors = @(t, q) [pose_error(t,q), or_error(t,q)];
-J4_tot_errors = @(t, q) [J4_pose_error(t,q), J4_orerr(t,q)];
-
-%% IK parameters
-qinit = [-pi/2, pi/4, 0, pi/3,0,0,0];
-
-t0 = 0;
-tf = 7;
-
 %% SOT (1 task)
-des_pos = @(t) despose(t);
-curr_pos = @(q) [indexAt(ForKine(q), 1:3, 4)', ...
-                    MatToRPY(indexAt(ForKine(q), 1:3, 1:3))];
 
-pose_error = @(t, q) curr_pos(q) - des_pos(t);
-
-% (-50; 0), but -10 is better
-K = -10 * eye(6); %-10 and 15s seems good-ish, lambda -1
-task1 = @(t, q) (K * pose_error(t, q)')';
-
-% -3 -3 "works" but it's not accurate
-% -2 -3 looks good [5s]
-% -2 -4 too
-% -2 -5, -3 -4 stops for tolerance
-options = odeset('RelTol', 1e-2, 'AbsTol', 1e-4);
-
-% better with -1; -5 is horrible
 [t1_sot, y1_sot] = ode15s(@(t, y) sot(t, y, {J}, {task1}, 10^(-1)),...
-                       [t0 tf], qinit, options);
+                       [t0 tf], qinit);
 
 q1_sot = y1_sot';
 
@@ -153,7 +199,7 @@ end
 
 sot1_err = zeros(6, size(q1_sot, 2));
 for k = 1:size(q1_sot, 2)
-    sot1_err(:, k) = tot_errors(t1_sot(k), q1_sot(:, k));
+    sot1_err(:, k) = EE_pose_errors(t1_sot(k), q1_sot(:, k));
 end
 
 sot1err_fig = figure2('Name', 'SoT (1 task) errors');
@@ -189,8 +235,8 @@ for i = 1:6
 end
 
 %% SoT
-[t_sot, y_sot] = ode15s(@(t,y) sot(t, y, {J, J4}, {task1, destwist4}, 10^(-1)),...
-                       [t0 tf], qinit, options);
+[t_sot, y_sot] = ode15s(@(t,y) sot(t, y, {J, J4}, {task1, task2}, 10^(-3)),...
+                       [t0 tf], qinit);
 
 q_sot = y_sot';
 
@@ -225,8 +271,8 @@ end
 sot_err = zeros(6, size(q_sot, 2));
 sot_err4 = zeros(6, size(q_sot, 2));
 for k = 1:size(q_sot, 2)
-    sot_err(:, k) = tot_errors(t_sot(k), q_sot(:, k));
-    sot_err4(:, k) = J4_tot_errors(t_sot(k), q_sot(:, k));
+    sot_err(:, k) = EE_pose_errors(t_sot(k), q_sot(:, k));
+    sot_err4(:, k) = J4_pose_errors(t_sot(k), q_sot(:, k));
 end
 
 soterr_fig = figure2('Name', 'SoT errors');
@@ -276,13 +322,10 @@ end
 % -1 -6, -1 -5, -1 -4, -1 -3, -1 -2, -1 -1  <- horrible
 % options2 = odeset('RelTol', 1e-5, 'AbsTol', 1e-1);
 %{
-% starting with 10 as above
-% tried but too slow in practice, so stopped:
-% -1, -100, -1000, 1, 10, 100, 1000
-K = -10 * eye(6); %
-task2 = @(t, q) (K * pose_error(t, q)')';
+K1_2 = 0.01 * eye(6); %
+task1_2 = @(t, q) (K1_2 * EE_pose_errors(t, q)')';
 
-[t1_rp, y1_rp] = ode45(@(t,y) rp(t, y, {J, J4}, {task2}, 10^(-2)),...
+[t1_rp, y1_rp] = ode45(@(t,y) rp(t, y, {J, J4}, {task1_2}, 10^(-5)),...
                        [t0 tf], qinit, options);
 
 q1_rp = y1_rp';
@@ -293,7 +336,7 @@ plot3(first_traj(:, 1), first_traj(:, 2), first_traj(:, 3), '-', 'Color', 'black
 hold on
 show(kuka, qinit');
 grid
-for k=[1:size(q1_rp, 2)]
+for k = 1:size(q1_rp, 2)
     fk = ForKine(q1_rp(:, k));
     rp1_position = fk(1:3, 4);
     plot3(rp1_position(1), rp1_position(2), rp1_position(3), 'o');
@@ -318,14 +361,11 @@ end
 
 %% Reverse Priority
 
-% with 1e-1 the errors have spikes
-[t_rp, y_rp] = ode45(@(t,y) rp(t, y, {J, J4}, {task1, destwist4}, 10^(-2)),...
-                       [t0 tf], qinit, options);
-[t_rpAlt, y_rpAlt] = ode45(@(t,y) rp(t, y, {J, J4}, {task1, destwist4}, 10^(-1)),...
-                       [t0 tf], qinit, options);
+% not much different from 10^-3 on
+[t_rp, y_rp] = ode45(@(t,y) rp(t, y, {J, J4}, {task1, task2}, 10^(-3)),...
+                       [t0 tf], qinit);
 
 q_rp = y_rp';
-q_rpAlt = y_rpAlt';
 
 rp_fig = figure2('Name', 'RP with ode15s');
 title('$$ \lambda = 10^{-1} $$', 'interpreter', 'latex');
@@ -333,7 +373,7 @@ plot3(first_traj(:, 1), first_traj(:, 2), first_traj(:, 3), '-', 'Color', 'black
 hold on
 show(kuka, qinit');
 grid
-for k=[1:size(q_rp, 2)]
+for k = 1:size(q_rp, 2)
     fk = ForKine(q_rp(:, k));
     rp_position = fk(1:3, 4);
     plot3(rp_position(1), rp_position(2), rp_position(3), 'o');
@@ -358,15 +398,8 @@ end
 rp_err = zeros(6, size(q_rp, 2));
 rp_err4 = zeros(6, size(q_rp, 2));
 for k = 1:size(q_rp, 2)
-    rp_err(:, k) = tot_errors(t_rp(k), q_rp(:, k));
-    rp_err4(:, k) = J4_tot_errors(t_rp(k), q_rp(:, k));
-end
-
-rp_errAlt = zeros(6, size(q_rpAlt, 2));
-rp_errAlt4 = zeros(6, size(q_rpAlt, 2));
-for k = 1:size(q_rpAlt, 2)
-    rp_errAlt(:, k) = tot_errors(t_rpAlt(k), q_rpAlt(:, k));
-    rp_errAlt4(:, k) = J4_tot_errors(t_rpAlt(k), q_rpAlt(:, k));
+    rp_err(:, k) = EE_pose_errors(t_rp(k), q_rp(:, k));
+    rp_err4(:, k) = J4_pose_errors(t_rp(k), q_rp(:, k));
 end
 
 rperr_fig = figure2('Name', 'RP errors');
@@ -383,44 +416,6 @@ for i = 1:6
     else % orientation error
         plot(t_rp, rad2deg(rem(rp_err(i, :), 2*pi)))
         plot(t_rp, rad2deg(rem(rp_err4(i, :), 2*pi)))
-
-        ylabel("error [deg]");
-
-    end
-
-    grid
-    legend("EE", "J4");
-    xlabel("time [s]");
-    switch i
-        case 1
-            title("X-axis position");
-        case 2
-            title("Y-axis position");
-        case 3
-            title("Z-axis position");
-        case 4
-            title("X-axis orientation");
-        case 5
-            title("Y-axis orientation");
-        case 6
-            title("Z-axis orientation");
-    end
-end
-
-rpAlterr_fig = figure2('Name', 'RP (alternate version) errors');
-for i = 1:6
-    sp_rp = subplot(6, 1, i);
-    hold on
-
-    if i <= 3 % position errors
-        plot(t_rpAlt, rp_errAlt(i, :))
-        plot(t_rpAlt, rp_errAlt4(i, :))
-
-        ylabel("error [m]");
-
-    else % orientation error
-        plot(t_rpAlt, rad2deg(rem(rp_errAlt(i, :), 2*pi)))
-        plot(t_rpAlt, rad2deg(rem(rp_errAlt4(i, :), 2*pi)))
 
         ylabel("error [deg]");
 
