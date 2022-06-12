d0ee8fa9838293b91615cb28307ef947b8796f0e
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-06-13 01:27:09 +0200

    Deleted unused files

--- animation_sot.m
+++
@@ -1,8 +0,0 @@
-function animation_sot(hObject, event, robot, pos)
-    val = round(get(event.AffectedObject, 'Value'));
-    ax = show(robot, pos(:,end,val));
-%     ax.XLim = [0, 0.8];%[-1,1];
-%     ax.YLim = [0, 0.8];%[-1,1];
-%     ax.ZLim = [0, 1]; %[-1,1];
-    disp(['pos= ' num2str(val)])
-end
--- manIndexDer.m
+++
@@ -1,9 +0,0 @@
-function varargout = manIndexDer(qq, jacobian, q_sym) 
-    dj = jacobian(q_sym);
-    for i=[1:size(q_sym,1)]
-        der_J(:,:,i) = diff(dj,q_sym(i));
-    end
-
-    a = matlabFunction(der_J(:,:,1));
-    a(qq(1), qq(2), qq(3), qq)
-end
