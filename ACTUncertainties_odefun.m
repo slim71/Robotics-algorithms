0c934118a25ba26f583780d7a9ea944980d9d2a3
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2022-08-10 22:12:54 +0200

    Added AdaptiveComputedTorque

---
+++ ACTUncertainties_odefun.m
@@ -0,0 +1,24 @@
+function unc_pars_dot = ACTUncertainties_odefun(t, y, q_array, des_array, R, P, robot)
+    
+    n = size(q_array, 2)/2; % or size(des_array, 2)
+
+    qs = q_array(1:n);
+    dqs = q_array(n+1:2*n);
+
+    % Simply any desired configuration derived from Cinematic Inversion
+    q_des = des_array(1:n)';
+    dq_des = des_array(n+1:2*n)';
+    ddq_des = des_array(2*n+1:end)';
+
+    % Errors
+    e = q_des - qs;
+    de = dq_des - dqs;
+
+    % Regressor
+    Y = DynamicRegressor(qs, dqs, dq_des, ddq_des);
+    
+    % Update law (u_pi) of estimated parameters
+    x = [e'; de'];
+    B = [zeros(n); eye(n)];
+    unc_pars_dot = inv(R) * Y' * pinv(robot.inertia(qs)') * B' * P * x;
+end
---
+++ ACT_odefun.m
@@ -0,0 +1,29 @@
+function ret_array = ACT_odefun(t, y, des_array, Kp, Kd, robot)
+
+    n = size(y, 1)/2; % or size(des_array, 2)
+
+    qs = y(1:n)';
+    dqs = y(n+1:2*n)';
+
+    qds = des_array(1:n)';
+    dqds = des_array(n+1:2*n)';
+    ddqds = des_array(2*n+1:end);
+    
+    M = robot.inertia(qs);
+    C = robot.coriolis(qs, dqs);
+    G = robot.gravload(qs);
+
+    e = (qds - qs);
+    de = (dqds - dqs);
+
+    % TODO
+    tau = M*(ddqds + Kd*de' + Kp*e') + C*dqs' + G';
+
+    % z = dq/dt
+    z = dqs;
+    % dz/dt = ddq/ddt
+    dzdt = pinv(M) * (tau - C*dqs' - G');
+    
+    % qdot, qddot
+    ret_array = [z'; dzdt];
+end
